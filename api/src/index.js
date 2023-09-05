bf313807e233d30ba26e8726c064d0123ac88b0d
DaviCavalcantiVital <davicvital@gmail.com>
2023-09-05 14:38:36 -0300

    ok

--- api/src/index.js
+++ api/src/index.js
@@ -2,3 +2,8 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 
+const server = express();
+server.use(cors());
+server.use(express.json())
+
+server.listen(process.env.port, () => console.log(`api conectada na ${process.env.PORT}`))
